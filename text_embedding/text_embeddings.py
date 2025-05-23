5fafe5380df5ff5a85134fcb7804d7c172b6ef37
Vijay <codewithvijay9@gmail.com>
2025-05-23 10:14:26 +0530

    Update text_embeddings.py

--- text_embedding/text_embeddings.py
+++ text_embedding/text_embeddings.py
@@ -1,9 +1,10 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import os
 
 
 class ChunkEmbeddings:
     def __init__(self):
-        self.embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        self.embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=os.getenv("GEMINI_API_KEY"))
     
     def get_embedding_model(self):
         return self.embedding_model
